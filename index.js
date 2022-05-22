ec1d6e46d20e2b131f23f9172e9465558f5db394
Katecr <katecr17@gmail.com>
2022-05-22 11:46:21 -0500

    [Add] config api in heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; //assign port if it comes from an environment variable otherwise assign 3000
 
 /**********middleware to receive json*************/
 app.use(express.json());
